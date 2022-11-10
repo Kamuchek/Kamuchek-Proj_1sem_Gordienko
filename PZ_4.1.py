23afdc0d721a90c74b0e58a138a4dcc1f0541d14
Kamuchek <115423982+Kamuchek@users.noreply.github.com>
2022-11-10 19:43:49 +0300

    Create PZ_4.1.py

---
+++ PZ_4.1.py
@@ -0,0 +1,12 @@
+import random
+A = random.randrange(1,11)
+B = random.randrange(A+1,21)
+print("A = ",A)
+print("B = ",B)
+print()
+K = 1
+for i in range(A,B+1):
+ for j in range(0,K):
+ print(i,end=" ")
+ print()
+ K += 1
