f38f8531a2656773bba7d859f8711d01ee54a50e
Kamuchek <115423982+Kamuchek@users.noreply.github.com>
2022-12-15 22:54:00 +0300

    Update PZ_5.2.py

--- PZ_5.2.py
+++ PZ_5.2.py
@@ -1,9 +1,17 @@
 import random
-N = random.randrange(2,21)
-#a = [random.randrange(1,11) for i in range(N)]
-a = [i for i in range(N)]
-b = a[0::3]
-print("N:",N)
-print("Array a:\n",a)
-print("Length of b:\n",len(b))
-print("Array b:\n",b)
+def RectPS(x1,y1,x2,y2,Result):
+    a = abs(x1-x2)
+    b = abs(y1-y2)
+    print("a = ", a)
+    print("b = ", b)
+    Result['P'] = 2*(a+b)
+    Result['S'] = a*b
+    return
+x1, x2 = random.sample(range(-10, 10), 2)
+y1, y2 = random.sample(range(-10, 10), 2)
+print("x1 = ",x1,"; y1 = ",y1)
+print("x2 = ",x2,"; y2 = ",y2)
+R = {'P' : None, 'S' : None}
+RectPS(x1,y1,x2,y2,R)
+print('P = ', R['P'])
+print('S = ', R['S'])
