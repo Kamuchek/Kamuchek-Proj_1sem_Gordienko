4866f42b54e3498be5714fddf9d2c076294d2b62
Kamuchek <115423982+Kamuchek@users.noreply.github.com>
2023-07-01 13:21:46 +0300

    Update KT_4_2_blob.js

--- KT_4_2_blob.js
+++ KT_4_2_blob.js
@@ -1,9 +1,9 @@
 // Вариант 24: Составить программу вычисления функции y = n∑i=1 (4bi^2)/(1+I cosi)
 function calculateY(n, b) {
-    var sum = 0;
+    let sum = 0;
 
-    for (var i = 1; i <= n; i++) {
-        var term = (4 * b * Math.pow(i, 2)) / (1 + i * Math.cos(i));
+    for (let i = 1; i <= n; i++) {
+        let term = (4 * b * Math.pow(i, 2)) / (1 + i * Math.cos(i));
         sum += term;
     }
 
@@ -11,8 +11,8 @@ function calculateY(n, b) {
 }
 
 // Пример
-var n = 5; // Количество итераций
-var b = 2; // Коэффициент
+let n = 5; // Количество итераций
+let b = 2; // Коэффициент
 
-var result = calculateY(n, b);
+let result = calculateY(n, b);
 console.log("Результат вычисления функции Y: " + result);
